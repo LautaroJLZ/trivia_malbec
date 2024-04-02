9f07a4fd605313af8d7a530c27a019032592f7a9
Herrera Daniel <danielherrera2009@hotmail.es>
2024-04-02 00:20:50 -0300

    login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,10 +12,10 @@ export default function Home() {
           <div className="w-[35%] backdrop-blur-sm bg-white/[.05] flex flex-col py-10 px-16 border border-white rounded-lg shadow-xl">
             <h1 className="text-left text-white text-4xl py-8">Login Malbec</h1>
             <label htmlFor="username" className="text-white">Usuario:</label>
-            <input id="username" type="text" className="text-red-800 border-2 border-white rounded-md py-1 px-2 my-2" />
+            <input id="username" type="text" className="text-red-800 border-2 border-white rounded py-2 px-5 my-2" />
             <label htmlFor="password" className="text-white mt-4">Contraseña:</label>
-            <input id="password" type="password" className="text-red-800 border-2 border-white rounded-md py-1 px-2 my-2" />
-            <Link href="/Init" className="bg-white text-red-700 rounded-md px-10 py-3 text-center my-10 text-2xl hover:bg-red-500 hover:text-white transition-all duration-300 cursor-pointer">
+            <input id="password" type="password" className="text-red-800 border-2 border-white rounded py-2 px-5 my-2" />
+            <Link href="/Init" className="bg-white text-red-700 rounded-md px-10 py-3 text-center my-10 text-xl hover:bg-red-500 hover:text-white transition-all duration-300 cursor-pointer">
               Iniciar Sesion 
             </Link>
           </div>
